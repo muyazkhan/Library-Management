3b30ac0cf59b40474c414322d7b40afcec484d88
Muyaz khan <muyazkhan06@gmail.com>
2024-01-01 13:48:52 +0600

    database add

--- Book_store/settings.py
+++ Book_store/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ['https://django-library-v00v.onrender.com/']

a2b1306ba2de85183f2b48a2b408281de16e775c
Muyaz khan <muyazkhan06@gmail.com>
2024-01-01 12:54:09 +0600

    database add

--- Book_store/settings.py
+++ Book_store/settings.py
@@ -144,7 +144,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'core','media')
 
-# CSRF_TRUSTED_ORIGINS = ['','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://djanhttps://django-library-v00v.onrender.com/','https://*.127.0.0.1']
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'

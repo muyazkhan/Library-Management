e75f992578068f2498c77bba11ab7a2ef295f70f
Muyaz khan <muyazkhan06@gmail.com>
2024-01-07 20:54:13 +0600

    reuplode

--- Book_store/urls.py
+++ Book_store/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
 from core.views import BookView
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include("accounts.urls")),
@@ -9,4 +12,6 @@ urlpatterns = [
     path('', BookView.as_view(),name='home'),
     path("books/<slug:book_slug>", BookView.as_view(), name="book_slug"),
 ]
-    
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
